63af195223a938e36c402da57d66897188c4a7db
chillhacker <gorohovdaniil19@gmail.com>
2025-01-20 00:48:36 +0300

    updated the hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,8 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  plugins: ["solidity-coverage"],
+  coverage: {
+    reporter: ["text-summary", "html"],
+  },
 };
